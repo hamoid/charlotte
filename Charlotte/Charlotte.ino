aa60bf8aeb0c1c527ce02ecec1db6b255b7761d5
Abe Pazos <abe@hamoid.com>
2015-12-03 17:14:37 +0100

    Event triggering when light delta changes direction

--- Charlotte/Charlotte.ino
+++ Charlotte/Charlotte.ino
@@ -1,13 +1,29 @@
 #include <Servo.h>
 
-int inVal, currVal;
+int currInVal, currVal;
+int len = 0;
+// Maybe the threshold should change to maintain a reasonable rate of events?
+const int lightChangeThreshold = 15;
+boolean goingUp = true;
 const int LEGS = 3;
 Servo servo[LEGS];
 
 int pos[LEGS] = { 20, 20, 20 };
-int avg[LEGS] = {  0,  0,  0 };
-int  in[LEGS] = {  0,  1,  7 };
-int out[LEGS] = {  2, 10, 12 };
+int avg[LEGS] = { 0, 0, 0 };
+int inPin[LEGS] = { 0, 1, 7 };
+int outPin[LEGS] = { 2,10, 12 };
+int lastInVal[LEGS] = { 80, 80, 80 };
+boolean lastGoingUp[LEGS] = { true, true, true };
+
+// To be used:
+// we need envelopes to alter speed and range
+// do we need current resting position, to tween towards that position?
+float speed = 1.0; 
+float range = 1.0;
+int modeCurr = 1;
+int modeNext = 1;
+float modeInterp = 0.5;
+
 // TODO
 // Keep list of recent events
 // 1. Walk - needs atan2
@@ -26,11 +42,10 @@ int out[LEGS] = {  2, 10, 12 };
 // Detect events. To do that, notice direction changes.
 
 long seconds=0;
-float speed = 0.002;
 
 void setup() {
   for(int i=0; i<LEGS; i++) {
-    servo[i].attach(out[i]);
+    servo[i].attach(outPin[i]);
   }
   Serial.begin(9600);
 }
@@ -40,7 +55,7 @@ void loop() {
   // update avg once per second
   if (millis() / 1000 > seconds){
     for(int i=0; i<LEGS; i++) {
-      avg[i] += analogRead(in[i]);
+      avg[i] += analogRead(inPin[i]);
       avg[i] /= 2;
     }
     seconds++;
@@ -48,21 +63,37 @@ void loop() {
   }  
 
   for(int i=0; i<LEGS; i++) {
-    inVal = analogRead(in[i]) - avg[i];
-    inVal = map(val, -800, 800, 0, 179);
-    pos[i] += constrain(inVal, 50, 140);
-    pos[i] /= 2;
-    
-    // currVal = pos[i];
+    currInVal = analogRead(inPin[i]) - avg[i];
+    currInVal = map(currInVal, -800, 800, 0, 179);
+    pos[i] += constrain(currInVal, 50, 140);
+    pos[i] /= 2;    
+    currInVal = pos[i];
     
     // yoga
-    // currVal = int(95 + 55 * (sin(millis() * speed)));
+    // currInVal = int(95 + 55 * (sin(millis() * speed)));
     
     // turn around. Sign of +i decides direction
     // the 0.005 decides the rotation speed
-    // currVal = int(95 + 55 * sin(millis() * 0.005 + i * 2.07));
+    // currInVal = int(95 + 55 * sin(millis() * 0.005 + i * 2.07));
     
-    servo[i].write(currVal);
+    //servo[i].write(currVal);
+    
+    goingUp = currInVal > lastInVal[i];
+    
+    if(goingUp == lastGoingUp[i]) {
+      len += abs(currInVal - lastInVal[i]);
+    } else {
+      // when the direction changes check
+      // if we traveled far
+      if(len > lightChangeThreshold) {
+        // trigger event
+        servo[i].write(random(50, 140));
+      }
+      len = 0;
+    }
+    
+    lastInVal[i] = currInVal;
+    lastGoingUp[i] = goingUp;
     
     delay(15);
   }
